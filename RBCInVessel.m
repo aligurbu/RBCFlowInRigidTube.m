f0b46e60438e904622e45f6321d70152121b0690
aligurbu <aligurbu@buffalo.edu>
2022-12-22 21:58:33 -0800

    Update RBCInVessel.m

--- RBCInVessel.m
+++ RBCInVessel.m
@@ -7,12 +7,24 @@
 %%
 clear all; close all; clc;
 addpath(genpath('../RBCFlowInRigidTube.m'))
+verbose_Plot = true;
 
 %% Input the model and parameters for the analysis from Models folder
-LoadElasRBC_Short_Pr4_2_Time0_75s
+% LoadElasRBC_Short_Pr4_2_Time0_75s
+LoadElasRBC_RefCons_6mic_Pr8
 
 %% Vessel set-up
 ParametersForTheAnalysis_Vessel
 
 %% RBC set-up
 ParametersForTheAnalysis_RBC
+
+if verbose_Plot
+    figure('Color','white')
+    hold on
+    VisualizeGeometry(nlat, nlon, aXi, bXi, 'r', true)
+    Patch_Mesh(coord, connect, 0.1)
+    axis on
+    xlabel('X'); ylabel('Y'); zlabel('Z');
+    view([0 90])
+end
