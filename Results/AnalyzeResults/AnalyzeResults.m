42e19d35fdc3c1f10e04eb418065f238ec219c28
aligurbu <aligurbu@buffalo.edu>
2022-12-23 02:54:37 -0800

    Create AnalyzeResults.m

---
+++ Results/AnalyzeResults/AnalyzeResults.m
@@ -0,0 +1,23 @@
+%% Analyze the results
+clear all; close all; clc;
+addpath(genpath('../../../RBCFlowInRigidTube.m'))
+
+%% Input the model and parameters for the analysis from Models folder
+LoadElasRBC_Short_Pr4_2_Time0_75s
+% LoadElasRBC_RefCons_6mic_Pr8
+% LoadElasRBC_LongConVes_Pr8
+
+% LoadMemVisRBC_Short_muMem10_Pr4_2_Time0_75
+% LoadMemVisRBC_RefCons_6mic_muMem_3_18_Pr8
+% LoadMemVisRBC_LongConVes_muMem_3_18_P40
+
+%% Set up output file
+fidCoord = fopen(['Coord_',name,'.dat'],'r');
+fidMemFor = fopen(['MemFor_',name,'.dat'],'r');
+fidSol = fopen(['Sol_',name,'.dat'],'r');
+fidTime = fopen(['Time_',name,'.dat'],'r');
+Time = fread(fidTime,'double');
+NSTEPS = length(Time);
+
+%%
+timeStepIncrement = 1;
